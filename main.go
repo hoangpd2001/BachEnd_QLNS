ad661b2d09753f485761398b23d4ec33c87d328b
hoangpd2001 <hoangpd2001@gmail.com>
2024-10-29 22:50:56 +0700

    Ff

--- main.go
+++ main.go
@@ -7,30 +7,27 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-//	"os"
+	"os"
 )
 
-	var E *echo.Echo
+	
 func main() {
 
 	sql := db.NewSqlConfig()
 	sql.Connect()
 	defer sql.Close()
-	E = echo.New() 
-	E.Use(middleware.AddTrailingSlash())
-	E.Use(middleware.Logger())
-	E.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	e := echo.New() 
+	e.Use(middleware.AddTrailingSlash())
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
 		AllowHeaders: []string{"Content-Type", "Authorization"},
 	}))
 
-	api := config.InitApp(E, sql.Db)
+	api := config.InitApp(e, sql.Db)
 	api.SetupRouter()
 
-	E.Logger.Fatal(E.Start(":1323"))
-	//e.Logger.Fatal(e.Start("0.0.0.0:" + os.Getenv("PORT")))
-}
-func GetEcho() *echo.Echo{
-	return E
+	//E.Logger.Fatal(E.Start(":1323"))
+	e.Logger.Fatal(e.Start("0.0.0.0:" + os.Getenv("PORT")))
 }
