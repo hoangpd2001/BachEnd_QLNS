934944bc7bbf33adf0444a30bc9c283b026dc9af
hoangpd2001 <hoangpd2001@gmail.com>
2024-11-05 12:05:28 +0700

    fix server

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"BackEnd/mod/config"
 	"BackEnd/mod/db"
 	"net/http"
+	"os"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-
 )
 
 func main() {
@@ -28,6 +28,6 @@ func main() {
 	api := config.InitApp(e, sql.Db)
 	api.SetupRouter()
 
-	e.Logger.Fatal(e.Start(":1323"))
-	//e.Logger.Fatal(e.Start("0.0.0.0:" + os.Getenv("PORT")))
+	//e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start("0.0.0.0:" + os.Getenv("PORT")))
 }
--- utils/convertDate.go
+++ utils/convertDate.go
@@ -11,15 +11,12 @@ type CustomDate struct {
 }
 
 func (cd *CustomDate) UnmarshalJSON(data []byte) error {
-	// Chuỗi để lưu ngày sau khi bỏ dấu ngoặc kép
 	var dateString string
 
-	// Giải mã JSON và lưu vào biến dateString
 	if err := json.Unmarshal(data, &dateString); err != nil {
 		return err
 	}
 
-	// Parse chuỗi ngày ("YYYY-MM-DD")
 	t, err := time.Parse("2006-01-02", dateString)
 	if err != nil {
 		return err
@@ -30,7 +27,6 @@ func (cd *CustomDate) UnmarshalJSON(data []byte) error {
 }
 
 func (cd CustomDate) MarshalJSON() ([]byte, error) {
-	// Chuyển thành chuỗi chỉ chứa ngày để trả về JSON ("YYYY-MM-DD")
 	formatted := fmt.Sprintf("\"%s\"", cd.Time.Format("2006-01-02"))
 	return []byte(formatted), nil
 }
